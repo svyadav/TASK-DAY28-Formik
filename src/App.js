bc4c299c08758fe4df305c0b1d82bd801c8876b6
sachin <ysachin511@gmail.com>
2022-09-09 22:46:47 +0530

    formik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./Profile";
+import AddUser from "./AddUser";
+import EditUser from "./EditUser";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/create" element={<AddUser />} />
+          <Route path="/create/:id" element={<EditUser />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/EditUser.js
@@ -0,0 +1,105 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Form, Input, Label, FormGroup, Button } from "reactstrap";
+import "./App.css";
+
+const EditUser = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const [editData, setEditData] = useState({
+    name: "",
+    email: "",
+    username: "",
+    address: "",
+  });
+
+  //   const [name, setName] = useState();
+
+  //   async function getData() {
+  //     const response = await axios.get(
+  //       "https://626e4477e58c6fabe2dd5cce.mockapi.io/profile/" + id
+  //     );
+  //     setName(response.data.name);
+  //   }
+
+  useEffect(() => {
+    axios
+      .get("https://626e4477e58c6fabe2dd5cce.mockapi.io/profile/" + id)
+      .then((response) => setEditData(response.data));
+  }, []);
+
+  const handleSubmit = async () => {
+    await axios.put(
+      "https://626e4477e58c6fabe2dd5cce.mockapi.io/profile/" + id,
+      editData
+    );
+
+    navigate("/");
+  };
+
+  const handleChange = (e) => {
+    setEditData({ ...editData, [e.target.name]: e.target.value });
+  };
+  return (
+    <>
+      <h1>Update profile</h1>
+      <Container>
+        <Form>
+          <FormGroup>
+            <Label for="name">Name</Label>
+            <Input
+              id="name"
+              name="name"
+              placeholder="Enter Name"
+              type="text"
+              className="text"
+              onChange={handleChange}
+              value={editData.name}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input
+              id="email"
+              name="email"
+              placeholder="Enter Email"
+              type="text"
+              className="text"
+              onChange={handleChange}
+              value={editData.email}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="username">Username</Label>
+            <Input
+              id="username"
+              name="username"
+              placeholder="Enter Username"
+              type="text"
+              onChange={handleChange}
+              value={editData.username}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Label for="address">Address</Label>
+            <Input
+              id="address"
+              name="address"
+              placeholder="Enter Address"
+              type="text"
+              onChange={handleChange}
+              value={editData.address}
+            />
+          </FormGroup>
+        </Form>
+      </Container>
+
+      <Button color="primary" onClick={() => handleSubmit()}>
+        Submit
+      </Button>
+    </>
+  );
+};
+
+export default EditUser;
